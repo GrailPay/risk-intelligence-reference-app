fbb08683e3761e3c6ba4f40412623c89133cfa3c
Senik Hakobyan <sen.hakobyan@gmail.com>
2025-01-14 18:12:38 +0400

    Refactor and enhance the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
-from flask import Flask
 from dotenv import load_dotenv
-
+from flask import Flask
 
 load_dotenv()
 
@@ -9,6 +8,8 @@ def create_app() -> Flask:
     app = Flask(__name__)
 
     from .routes import main
+
     app.register_blueprint(main)
+    app.config.from_object("config.Config")
 
     return app
